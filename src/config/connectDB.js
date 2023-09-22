5094faeeedac015e85b65d7e64ecc78e3595cc06
hangduchuy <duchuy246938@gmail.com>
2023-09-22 10:30:27 +0700

    merge xong va database moi

--- src/config/connectDB.js
+++ src/config/connectDB.js
@@ -1,7 +1,7 @@
 const { Sequelize } = require('sequelize');
 
 // Option 3: Passing parameters separately (other dialects)
-const sequelize = new Sequelize('bookingcare', 'root', '8643947', {
+const sequelize = new Sequelize('bookingcare', 'root', '246938', {
     host: 'localhost',
     dialect: 'mysql',
     logging: false
--- src/models/handBook.js
+++ src/models/handBook.js
@@ -11,7 +11,7 @@ module.exports = (sequelize, DataTypes) => {
          */
         static associate(models) {
             // define association here
-            Handbook.hasMany(models.Doctor_Infor, { foreignKey: 'handBookId' })
+            // Handbook.hasMany(models.Doctor_Infor, { foreignKey: 'handBookId' })
         }
     };
     Handbook.init({
