3257fdb42941496957ed94125d0b72fc920d980b
hangduchuy <duchuy246938@gmail.com>
2023-10-16 18:27:38 +0700

    test

--- src/config/config.js
+++ src/config/config.js
@@ -1,30 +1,58 @@
 require('dotenv').config()
 
 module.exports = {
-  "development": {
-    "username": process.env.DB_USERNAME,
-    "password": process.env.DB_PASSWORD,
-    "database": process.env.DB_DATABASE_NAME,
-    "host": process.env.DB_HOST,
-    "dialect": "mysql",
-    "logging": false,
-    "query": {
+  development: {
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DATABASE_NAME,
+    host: process.env.DB_HOST,
+    dialect: "postgres",
+    logging: false,
+    query: {
       "raw": true
     },
-    "timezone": "+07:00"
+    timezone: "+07:00",
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false
+      }
+    }
   },
-  "test": {
-    "username": process.env.DB_USERNAME,
-    "password": process.env.DB_PASSWORD,
-    "database": process.env.DB_DATABASE_NAME,
-    "host": process.env.DB_HOST,
-    "dialect": "mysql"
+  test: {
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DATABASE_NAME,
+    host: process.env.DB_HOST,
+    dialect: "postgres",
+    logging: false,
+    query: {
+      "raw": true
+    },
+    timezone: "+07:00",
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false
+      }
+    }
   },
-  "production": {
-    "username": process.env.DB_USERNAME,
-    "password": process.env.DB_PASSWORD,
-    "database": process.env.DB_DATABASE_NAME,
-    "host": process.env.DB_HOST,
-    "dialect": "mysql"
+  production: {
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DATABASE_NAME,
+    host: process.env.DB_HOST,
+    dialect: "postgres",
+    logging: false,
+    query: {
+      "raw": true
+    },
+    timezone: "+07:00",
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false
+      }
+    }
   }
 }
--- src/config/connectDB.js
+++ src/config/connectDB.js
@@ -4,8 +4,18 @@ const { Sequelize } = require('sequelize');
 const sequelize = new Sequelize(process.env.DB_DATABASE_NAME, process.env.DB_USERNAME,
     process.env.DB_PASSWORD, {
     host: process.env.DB_HOST,
-    dialect: 'mysql',
-    logging: false
+    dialect: 'postgres',
+    logging: false,
+    dialectOptions: {
+        ssl: {
+            require: true,
+            rejectUnauthorized: false
+        }
+    },
+    query: {
+        "raw": true
+    },
+    timezone: "+07:00"
 });
 
 
