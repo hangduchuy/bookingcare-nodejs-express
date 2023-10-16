b24a9ce1cae4ed93a43d612dbca74fe36d1f8625
hangduchuy <duchuy246938@gmail.com>
2023-10-16 19:37:01 +0700

    tet

--- src/controllers/homeControllers.js
+++ src/controllers/homeControllers.js
@@ -1,11 +1,9 @@
-import db from '../models/index';
 import CRUDservice from '../services/CRUDService';
 
 let getHomePage = async (req, res) => {
     try {
-        let data = await db.User.findAll();
         return res.render('homepage.ejs', {
-            data: JSON.stringify(data)
+            data: JSON.stringify({})
         });
     } catch (e) {
         console.log(e)
